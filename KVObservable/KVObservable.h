133a9ca94517362144fb9e33403c1c050be07c22
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2016-03-17 22:44:17 +0900

    Add the Xcode project

---
+++ KVObservable/KVObservable.h
@@ -0,0 +1,19 @@
+//
+//  KVObservable.h
+//  KVObservable
+//
+//  Created by Kazunobu Tasaka on 3/17/16.
+//  Copyright © 2016 Kazunobu Tasaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVObservable.
+FOUNDATION_EXPORT double KVObservableVersionNumber;
+
+//! Project version string for KVObservable.
+FOUNDATION_EXPORT const unsigned char KVObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVObservable/PublicHeader.h>
+
+
