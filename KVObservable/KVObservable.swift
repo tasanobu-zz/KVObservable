72688eaf900d95ac69d7a919a5beaa64b040c877
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2016-03-20 21:19:22 +0900

    Implement KeyObserverType and TargetObserverType

--- KVObservable/KVObservable.swift
+++ KVObservable/KVObservable.swift
@@ -8,26 +8,76 @@
 
 import Foundation
 
-protocol Observable {
+
+
+public protocol KeyObserverType {
+    typealias Target: NSObject
+    typealias Value
+    
+    var keyPath: String { get }
+    var target: Target { get }
+    var valueChangeHandler: (old: Value?, new: Value?) -> () { get }
+}
+
+public final class AnyKeyObserver<Target: NSObject, Value>: NSObject, KeyObserverType {
+    
+    public let target: Target
+    public let keyPath: String
+    public let valueChangeHandler: (old: Value?, new: Value?) -> ()
+    
+    public init<Inner: KeyObserverType where Target == Inner.Target, Value == Inner.Value>(_ inner: Inner) {
+        self.target = inner.target
+        self.keyPath = inner.keyPath
+        self.valueChangeHandler = inner.valueChangeHandler
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    public func resume() {
+        let options: NSKeyValueObservingOptions = [.New, .Old]
+        target.addObserver(self, forKeyPath: keyPath, options: options, context: nil)
+    }
+    
+    private func stop() {
+        target.removeObserver(self, forKeyPath: keyPath)
+    }
+    
+    // MARK: KVO
+    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        guard let keyPath = keyPath,
+            let change = change
+            where target === object else { return }
+        
+        if keyPath == self.keyPath {
+            let old = change[NSKeyValueChangeOldKey] as? Value
+            let new = change[NSKeyValueChangeNewKey] as? Value
+            valueChangeHandler(old: old, new: new)
+        }
+    }
+}
+
+public struct ValueChange {
+    let keyPath: String
+    let oldValue: AnyObject?
+    let newValue: AnyObject?
+}
+
+public protocol TargetObserverType {
     typealias Target: NSObject
     var target: Target { get }
     var keyPaths: [String] { get }
     var valueChangeHandler: (ValueChange) -> () { get }
 }
 
-struct ValueChange {
-    let keyPath: String
-    let oldValue: AnyObject?
-    let newValue: AnyObject?
-}
-
 // MARK:
-class AnyObservable<Target: NSObject>: NSObject, Observable {
-    let target: Target
-    let keyPaths: [String]
-    let valueChangeHandler: (ValueChange) -> ()
+public class AnyTargetObserver<Target: NSObject>: NSObject, TargetObserverType {
+    public let target: Target
+    public let keyPaths: [String]
+    public let valueChangeHandler: (ValueChange) -> ()
     
-    init<Base: Observable where Target == Base.Target>(_ base: Base) {
+    public init<Base: TargetObserverType where Target == Base.Target>(_ base: Base) {
         self.target = base.target
         self.keyPaths = base.keyPaths
         self.valueChangeHandler = base.valueChangeHandler
@@ -37,9 +87,9 @@ class AnyObservable<Target: NSObject>: NSObject, Observable {
         stop()
     }
     
-    func resume() {
+    public func resume() {
         keyPaths.forEach {
-            let options = NSKeyValueObservingOptions(arrayLiteral: .New, .Old)
+            let options: NSKeyValueObservingOptions = [.New, .Old]
             target.addObserver(self, forKeyPath: $0, options: options, context: nil)
         }
     }
@@ -51,7 +101,7 @@ class AnyObservable<Target: NSObject>: NSObject, Observable {
     }
 
     // MARK: KVO
-    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
         guard let keyPath = keyPath,
             let change = change
             where target === object else { return }
